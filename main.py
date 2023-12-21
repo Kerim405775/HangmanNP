385e8ff73e36fd3f3f5b4d9d692ceb20dfba9ed4
mirkosms <miroslawsmoronski+git@gmail.com>
2023-12-21 11:38:59 +0100

    Add project with almost ready code for console play

---
+++ main.py
@@ -0,0 +1,68 @@
+import random
+from hangman_words import word_list
+from hangman_art import logo, stages
+from hangman_ranking import save_score, print_ranking
+
+def main_menu():
+    while True:
+        print("1. Start new game")
+        print("2. Show ranking")
+        print("3. Quit")
+        choice = input("Choose an option: ")
+        if choice == '1':
+            start_game()
+        elif choice == '2':
+            print_ranking()
+        elif choice == '3':
+            break
+        else:
+            print("Invalid option. Please try again.")
+
+def start_game():
+    player_name = input("Enter your name: ")
+    print(logo)
+
+    chosen_word = random.choice(word_list)
+    word_length = len(chosen_word)
+
+    end_of_game = False
+    lives = 6
+
+    display = []
+    for _ in range(word_length):
+        display += "_"
+
+    guessed_letters = []
+
+    while not end_of_game:
+        guess = input("Guess a letter: ").lower()
+
+        if guess in guessed_letters:
+            print("You've already guessed this letter.")
+        else:
+            guessed_letters.append(guess)
+
+            for position in range(word_length):
+                letter = chosen_word[position]
+                if letter == guess:
+                    display[position] = letter
+
+            if guess not in chosen_word:
+                print("This letter is not in the word.")
+                lives -= 1
+                if lives == 0:
+                    end_of_game = True
+                    print("You lose.")
+
+        print(f"{' '.join(display)}")
+
+        if "_" not in display:
+            end_of_game = True
+            print("You win.")
+
+        print(stages[lives])
+
+    save_score(player_name, lives)
+
+if __name__ == "__main__":
+    main_menu()
